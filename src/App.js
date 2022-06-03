6806bc6374dfbbf72e298f94ae1c5199092b8c98
bmykhaylivvv <bmykhaylivvv@gmail.com>
2022-06-04 01:01:49 +0300

    Minor edits in App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import SignInPage from "./pages/SignInPage";
 import Profile from "./pages/Profile";
 import MyProfilePage from "./pages/MyProfilePage";
 import AddPostPage from "./pages/AddPostPage"
+import { Navigate } from "react-router-dom";
+
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigate to="/feed" replace={true} />} />
           <Route path="/feed" element={<HomePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/signin" element={<SignInPage />} />
