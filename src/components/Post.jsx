0caf0ed1544c9e88e8efbca65f56f8a40a7223b4
kkulykk <kkulykk@gmail.com>
2022-06-04 00:59:47 +0300

    Add Feed page

---
+++ src/components/Post.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import { red } from "@mui/material/colors";
+
+const Post = (props) => {
+  return (
+    <Card sx={{ maxWidth: 450, maxHeight: 450 }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            sx={{ bgcolor: red[500] }}
+            alt={props.userName}
+            aria-label="post"
+            src={props.userPhoto}
+          />
+        }
+        title={props.userName}
+      />
+      <CardMedia component="img" height="250" image={props.photo} alt="Photo" />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.text}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Post;
