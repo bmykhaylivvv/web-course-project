41244dfed2a2189091eaf0d0912c0491ee7f0546
bmykhaylivvv <bmykhaylivvv@gmail.com>
2022-06-04 01:46:24 +0300

    Edit logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ const Header = () => {
               textDecoration: "none",
             }}
           >
-            LOGO
+            Metoo
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
