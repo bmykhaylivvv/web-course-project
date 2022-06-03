c7d771ec242d426bc361ec7bba668c7533f7552b
kkulykk <kkulykk@gmail.com>
2022-06-03 23:38:45 +0300

    Add Profile page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/feed" element={<HomePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/signin" element={<SignInPage />} />
+          <Route path="/:userName" element={<Profile />} />
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
 
 import { firebaseAuth } from "../config/firebase-config";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const logOut = async () => {
-        try {
-            await firebaseAuth.signOut();
-            navigate("/signin");
-        } catch (err) {
-            // handle error
-        }
-    };
+  const logOut = async () => {
+    try {
+      await firebaseAuth.signOut();
+      navigate("/signin");
+    } catch (err) {
+      // handle error
+    }
+  };
 
-    useEffect(() => {
-        firebaseAuth.onAuthStateChanged(async (userCred) => {
-            if (!userCred) {
-                navigate("/signin");
-            }
-        });
-    }, []);
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged(async (userCred) => {
+      if (!userCred) {
+        navigate("/signin");
+      }
+    });
+  }, []);
 
-    return (
-        <div>
-            <p>Home111</p>
-            <button onClick={() => logOut()}>Log Out</button>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <p>Home111</p>
+    </div>
+  );
 };
 
 export default Home;
